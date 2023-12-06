2fabba988c22653cf60a8b74e20c5dcd2981a211
Fhemiyung <152605068+Fhemiyung@users.noreply.github.com>
2023-12-06 12:08:38 +0100

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,20 @@
+# variable naming can not start with a digit
+# variable naming cannot have any special character
+# variable naming cannot contain a space 
+num1 = 14 
+num2 = 25.6 
+name = ‘simeon’
+x  = True 
+word  = ‘We\ ‘re are brothers from the town’
+print {word} 
+word2 =  ‘python is fun\npython is easy\npython is great’
+print {word2}#\n denotes new line
+word3 = ‘’’python is fun
+python is easy
+python is great
+‘’’
+print {word3}
+
+print{ ‘Hello’  + ‘ World ‘}
+print { ‘My name is ‘  + name}
+
